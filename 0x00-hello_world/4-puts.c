49fdba3539131d52dd7218f2380288fac9a20701
FelicityOdera <felicity.odera@gmail.com>
2022-06-02 10:59:41 -0700

    C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Entry point
+ * Description: printing programming is like building a multilingual puzzle
+ * Return: 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
