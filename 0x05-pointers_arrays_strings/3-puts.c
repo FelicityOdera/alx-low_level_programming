2eba6604ca59ac154fcd35112889531fa5a69f56
FelicityOdera <felicity.odera@gmail.com>
2022-06-13 06:52:14 -0700

    function that prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 /**
  * _puts - Function that prints a string, followed by a new line
- *
- * @str: String to be printed
+ * @str: value to be evaluate
+ * Return: Nothing
  */
 void _puts(char *str)
 {
-while (*str)
-	_putchar(*str++);
-	_putchar('\n');
+int l = 0;
+while (*(str + l) != '\0')
+{
+	_putchar(str[l]);
+	l++
+}
+_putchar('\n');
 }
