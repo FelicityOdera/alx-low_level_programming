5ceb7d4addc6ad473782d34c5aa9ae3dd07e3d44
FelicityOdera <felicity.odera@gmail.com>
2022-06-22 07:53:08 -0700

    function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,12 @@
 #include "main.h"
-/*
+/**
  * _puts_recursion - prints a string, followed by a new line
- * @s: string
- * Return: 0
+ * @s: pointer to string
+ * Return: nothing
  */
-
 void _puts_recursion(char *s)
 {
-if (*s != '\0')
+if (*s == '\0')
 {
 	_putchar(*s);
 	_puts_recursion(s + 1);
