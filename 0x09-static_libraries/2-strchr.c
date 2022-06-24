7d5b6d54579f440ee7c9a99e467b5ccba05cdaf2
FelicityOdera <felicity.odera@gmail.com>
2022-06-24 16:29:21 -0700

    static library libmy.a containing all the functions

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -7,12 +7,18 @@
  */
 char *_strchr(char *s, char c)
 {
-unsigned int i = 0;
-
-for (; *(s + i) != '\0'; i++)
-if (*(s + i) == c)
-	return (s + i);
-if (*(s + i) == c)
-	return (s + i);
-return ('\0');
+while (*s != '\0') /*Declaring WHILE*/
+{
+if (*s == c) /*if s ==  c*/
+{
+	return (s); /*return s*/
 }
+++s;
+}
+if (*s == c)
+{
+	return (s);
+}
+return (0);/* values null*/
+}
+
