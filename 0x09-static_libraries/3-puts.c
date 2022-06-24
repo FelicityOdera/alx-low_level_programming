e3f5aea082cd737abb378727f9b38afaf24b994f
FelicityOdera <felicity.odera@gmail.com>
2022-06-24 15:54:11 -0700

    static library libmy.a containing all the functions

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _puts - Function that prints a string, followed by a new line
+ * @str: value to be evaluate
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int l = 0;
+
+	while (*(str + l) != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+	_putchar('\n');
+}
