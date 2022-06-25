4473d36955240ac547959050b0ef366b9865aad1
FelicityOdera <felicity.odera@gmail.com>
2022-06-24 17:02:39 -0700

    static library libmy.a containing all the functions

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _atoi - converts a string to an integer
  * @s: input string
@@ -10,22 +9,23 @@ int _atoi(char *s)
 unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
 while (*(s + count) != '\0')
+{
 if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 	break;
-	if (*(s + count) == '-')
-		pn *= -1;
-	if ((*(s + count) >= '0') && (*(s + count) <= '9'))
-	{
-		if (size > 0)
-			m *= 10;
-		size++;
-	}
-	count++;
+if (*(s + count) == '-')
+	pn *= -1;
+if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+{
+if (size > 0)
+	m *= 10;
+size++;
+}
+count++;
 }
 for (i = count - size; i < count; i++)
 {
-	oi = oi + ((*(s + i) - 48) * m);
-	m /= 10;
+oi = oi + ((*(s + i) - 48) * m);
+m /= 10;
 }
-return (oi * pn);
+return (oi *pn);
 }
