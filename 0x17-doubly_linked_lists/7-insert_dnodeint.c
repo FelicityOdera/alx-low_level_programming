2eb6638031820c882892477cc7634732e4137b54
FelicityOdera <87133135+FelicityOdera@users.noreply.github.com>
2022-09-01 20:36:58 +0300

    inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n, new->prev = new->next = NULL;
-  if (idx == 0)
+	if (idx == 0)
 	{
 		if (*head)
 	{
